a899f894011b93f11cc276c5e72f036e41b47c53
vikas seth <vikas.seth@mullenlowelintas.in>
2025-07-21 17:04:27 +0530

    chenge website

--- src/app/Core/models/message.model.ts
+++
@@ -1,7 +0,0 @@
-import { SafeHtml } from "@angular/platform-browser";
-
-export interface Message {
-    sender: 'user' | 'bot';
-    text: string | SafeHtml;
-    time?: string; // Optional field for the message timedfsd
-  }
--- src/app/Core/models/user.model.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-    id: number; // Or whatever unique identifier you use
-    name: string;
-    img: string; // URL to the user's image
-  }
